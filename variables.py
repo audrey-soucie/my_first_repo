c40194856f741b977288cffb5fcc446c4e76f761
audrey-soucie <97488913+audrey-soucie@users.noreply.github.com>
2022-02-02 18:33:20 -0700

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,12 @@
+# This is a comment
+# Variables in Python
+# By: Audrey Soucie
+# CS 1030
+
+# Variables
+
+first_name = 'Audrey'
+last_name = 'Soucie'
+
+print (first_name, last_name)
+print ('Mr. ' + first_name + '' + last_name)
